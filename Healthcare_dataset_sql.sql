0d9075023277fc4e36f77258bd71fe6d4f20de68
Fahim <fahimyadgir07@gmail.com>
2026-05-29 23:59:12 +0530

    update some information and delet specific row from dataset and change colunm name

--- Healthcare_dataset_sql.sql
+++ Healthcare_dataset_sql.sql
@@ -3,3 +3,47 @@ use healthcare;
 
 select * from healtcare_dataset;
 
+alter table healtcare_dataset
+rename column ï»¿Patient_name to name;
+
+alter table healtcare_dataset
+rename column `Blood Type` TO Blood_Type;
+
+alter table healtcare_dataset
+rename column `Medical Condition` to Medical_Condition;
+
+alter table healtcare_dataset
+rename column `Date of Admission` to Date_of_Admission;
+
+alter table healtcare_dataset
+rename column `Insurance Provider` to Insurance_Provider;
+
+alter table healtcare_dataset
+rename column `Billing Amount` to Billing_Amount;
+
+alter table healtcare_dataset
+modify column Age int;
+
+select * from healtcare_dataset
+where Age = '' or Age is null;
+
+alter table healtcare_dataset
+rename column `Room Number` to Room_Number;
+
+alter table healtcare_dataset
+rename column `Admission Type` to  Admission_Type;
+
+alter table healtcare_dataset
+rename column `Discharge Date` to  Discharge_Date;
+
+alter table healtcare_dataset
+rename column `Test Results` to Test_Results;
+
+SET SQL_SAFE_UPDATES = 0;
+
+alter table healtcare_dataset
+modify column Date_of_Admission date;
+
+
+DELETE FROM healtcare_dataset
+WHERE Name = '' OR Name IS NULL;
